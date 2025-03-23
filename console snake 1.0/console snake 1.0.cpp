148139faa4c2ba02add61f79d700564fe59caa61
NikitaCherskov <nikitacherskov@gmail.com>
2025-03-23 17:12:45 +0300

    Переименовал функции и переменные

--- console snake 1.0/console snake 1.0.cpp
+++ console snake 1.0/console snake 1.0.cpp
@@ -7,11 +7,10 @@
 #include <fstream>
 #include <cstdlib>
 #include <unordered_map>
-int polsiz = 16;
-int snakespeed = 2;
-int dbarrier = 0;
-int numskin = 1;
-int squareps;
+int FIELD_SIZE = 16;
+int SNAKE_SPEED = 2;
+int HAS_BARRIER = 0;
+int SKIN_NUMBER = 1;
 
 
 
@@ -58,7 +57,7 @@ void block::move(int lr, int du)
 {
     lastx = x;
     lasty = y;
-    if (x == polsiz - 1 && lr == 1)
+    if (x == FIELD_SIZE - 1 && lr == 1)
     {
         x = 0;
     }
@@ -66,14 +65,14 @@ void block::move(int lr, int du)
     {
         if (x == 0 && lr == -1)
         {
-            x = polsiz - 1;
+            x = FIELD_SIZE - 1;
         }
         else
         {
             x = x + lr;
         }
     }
-    if (y == polsiz - 1 && du == 1)
+    if (y == FIELD_SIZE - 1 && du == 1)
     {
         y = 0;
     }
@@ -81,7 +80,7 @@ void block::move(int lr, int du)
     {
         if (y == 0 && du == -1)
         {
-            y = polsiz - 1;
+            y = FIELD_SIZE - 1;
         }
         else
         {
@@ -177,43 +176,43 @@ void PrintMenuUnit(const char* text, int number, int choosen) {
 
 
 //---------------сохранения и загрузки-----------------
-void sav(int* bestscore)
+void saveGame(int* bestscore)
 {
     std::ofstream saving;
     saving.open("save.snake", std::ios::out | std::ios::binary);
-    saving.write((const char*)&polsiz, sizeof polsiz);
-    saving.write((const char*)&snakespeed, sizeof snakespeed);
-    saving.write((const char*)&dbarrier, sizeof dbarrier);
-    saving.write((const char*)&numskin, sizeof numskin);
+    saving.write((const char*)&FIELD_SIZE, sizeof FIELD_SIZE);
+    saving.write((const char*)&SNAKE_SPEED, sizeof SNAKE_SPEED);
+    saving.write((const char*)&HAS_BARRIER, sizeof HAS_BARRIER);
+    saving.write((const char*)&SKIN_NUMBER, sizeof SKIN_NUMBER);
     saving.write((const char*)bestscore, sizeof *bestscore);
     saving.close();
 }
 
 
 
-void lod(int* bestscore)
+void loadGame(int* bestscore)
 {
     std::ifstream loading;
     loading.open("save.snake", std::ios::in | std::ios::binary);
-    loading.read((char*)&polsiz, sizeof polsiz);
-    loading.read((char*)&snakespeed, sizeof snakespeed);
-    loading.read((char*)&dbarrier, sizeof dbarrier);
-    loading.read((char*)&numskin, sizeof numskin);
+    loading.read((char*)&FIELD_SIZE, sizeof FIELD_SIZE);
+    loading.read((char*)&SNAKE_SPEED, sizeof SNAKE_SPEED);
+    loading.read((char*)&HAS_BARRIER, sizeof HAS_BARRIER);
+    loading.read((char*)&SKIN_NUMBER, sizeof SKIN_NUMBER);
     loading.read((char*)bestscore, sizeof * bestscore);
     loading.close();
 }
 
 
 
-void clear(int* bestscore)
+void clearSave(int* bestscore)
 {
     std::fstream clear;
     clear.open("save.snake", std::ios::trunc | std::ios::binary);
     clear.close();
-    polsiz = 16;
-    snakespeed = 2;
-    dbarrier = 0;
-    numskin = 1;
+    FIELD_SIZE = 16;
+    SNAKE_SPEED = 2;
+    HAS_BARRIER = 0;
+    SKIN_NUMBER = 1;
     *bestscore = 0;
 }
 
@@ -222,20 +221,20 @@ void clear(int* bestscore)
 
 
 //-----------------------------------------------------
-void spawn_location_selection(int* x, int* y, int place[32][32])
+void spawnLocationSelection(int* x, int* y, int place[32][32])
 {
     int randcell;
     int i;
     int a;
     int b;
     int amount = 0;
-    cell* emty_place = new cell[polsiz * polsiz];
+    cell* emty_place = new cell[FIELD_SIZE * FIELD_SIZE];
     i = 0;
     a = 0;
-    while (a < polsiz)
+    while (a < FIELD_SIZE)
     {
         b = 0;
-        while (b < polsiz)
+        while (b < FIELD_SIZE)
         {
             if (place[a][b] == 0)
             {
@@ -257,19 +256,19 @@ void spawn_location_selection(int* x, int* y, int place[32][32])
 
 
 //Обработка смерти-------------------------------------
-int rest(int a)
+int cycleOnField(int a)
 {
-    if (a == polsiz)
+    if (a == FIELD_SIZE)
     {
         a = 0;
     }
     else if(a == -1)
     {
-        a = polsiz - 1;
+        a = FIELD_SIZE - 1;
     }
     return a;
 }
-int dead(block snakeb, int place[32][32], int l)
+int checkCollide(block snakeb, int place[32][32], int l)
 {
     int x_delta = 0;
     int y_delta = 0;
@@ -285,19 +284,18 @@ int dead(block snakeb, int place[32][32], int l)
     else {
         x_delta = -1;
     }
-    std::cout << x_delta << ";" << y_delta;
     int x_next = snakeb.x + x_delta;
     int y_next = snakeb.y + y_delta;
-    if (dbarrier == 1)
+    if (HAS_BARRIER == 1)
     {
-        if (x_next == -1 || x_next == polsiz) {
+        if (x_next == -1 || x_next == FIELD_SIZE) {
             return 1;
         }
-        if (y_next == -1 || y_next == polsiz) {
+        if (y_next == -1 || y_next == FIELD_SIZE) {
             return 1;
         }
     }
-    return place[rest(y_next)][rest(x_next)];
+    return place[cycleOnField(y_next)][cycleOnField(x_next)];
 }
 
 
@@ -305,7 +303,7 @@ int dead(block snakeb, int place[32][32], int l)
 
 
 //-----------------------------------------------------
-void fullzapis(char* inpstr, const char* outstr, int inlenth)
+void textCopy(char* inpstr, const char* outstr, int inlenth)
 {
     int inlf = 0;
     while (*outstr != '\0' && inlf < inlenth - 1)
@@ -325,7 +323,7 @@ void fullzapis(char* inpstr, const char* outstr, int inlenth)
 
 
 
-void zapis(char skin[4], const char setch[4])
+void skinTextCopy(char skin[4], const char setch[4])
 {
     int i = 0;
     while (i < 4)
@@ -347,12 +345,12 @@ void setskin(char skin[4])
         i++;
     }
     const char* skins[10] = {"|-|", "|=|", "|+|", "[-]", "[=]", "[+]", "(-)", "(=)" , "(+)", "111"};
-    zapis(skin, skins[numskin - 1]);
+    skinTextCopy(skin, skins[SKIN_NUMBER - 1]);
 }
 
 
 
-void char_to_int_adder(char* canvas, int toadd)
+void addIntToChar(char* canvas, int toadd)
 {
     std::string char_num = { "" };
     int i = 0;
@@ -372,7 +370,25 @@ void char_to_int_adder(char* canvas, int toadd)
 
 
 
-void setrank(char* nameskin, int can, int need_record)
+void addCharToChar(char* canvas, const char str_for_add[])
+{
+    int i = 0;
+    int i2 = 0;
+    while (canvas[i] != '\0')
+    {
+        i++;
+    }
+    while (str_for_add[i2] != '\0')
+    {
+        canvas[i] = str_for_add[i2];
+        i++;
+        i2++;
+    }
+}
+
+
+
+void setRank(char* nameskin, int can, int need_record)
 {
     const char* ranks[10] = {
         "Newbie",
@@ -388,12 +404,12 @@ void setrank(char* nameskin, int can, int need_record)
     };
     if (can == 1)
     {
-        fullzapis(nameskin, ranks[numskin], 64);
+        textCopy(nameskin, ranks[SKIN_NUMBER], 64);
     }
     else
     {
-        fullzapis(nameskin, "Locked, need score: ", 64);
-        char_to_int_adder(nameskin, need_record);
+        textCopy(nameskin, "Locked, need score: ", 64);
+        addIntToChar(nameskin, need_record);
     }
     return;
 }
@@ -405,28 +421,28 @@ void drawSettings(int vibor2, char skin[4], int bestscore, short need_record[10]
     bool can = 0;
     char name_skin[64]{ "" };
     char setiskin[3][4];
-    if (bestscore < need_record[numskin - 1])
+    if (bestscore < need_record[SKIN_NUMBER - 1])
     {
-        zapis(setiskin[0], "X X");
-        zapis(setiskin[1], " X ");
-        zapis(setiskin[2], "X X");
+        skinTextCopy(setiskin[0], "X X");
+        skinTextCopy(setiskin[1], " X ");
+        skinTextCopy(setiskin[2], "X X");
         can = 0;
     }
     else
     {
-        zapis(setiskin[0], skin);
-        zapis(setiskin[1], skin);
-        zapis(setiskin[2], skin);
+        skinTextCopy(setiskin[0], skin);
+        skinTextCopy(setiskin[1], skin);
+        skinTextCopy(setiskin[2], skin);
         can = 1;
     }
-    setrank(name_skin, can, need_record[numskin - 1]);
+    setRank(name_skin, can, need_record[SKIN_NUMBER - 1]);
     std::cout << "SETTINGS:\n";
     PrintMenuUnit("size of place", 1, vibor2);
-    std::cout << "\t\t< " << polsiz << " X " << polsiz << " >" << "\t" << setiskin[0] << "\n";
+    std::cout << "\t\t< " << FIELD_SIZE << " X " << FIELD_SIZE << " >" << "\t" << setiskin[0] << "\n";
     PrintMenuUnit("speed of snake", 2, vibor2);
-    std::cout << "\t\t< " << snakespeed << " m/s >" << "\t" << setiskin[1] << "\t" << name_skin << "\n";
+    std::cout << "\t\t< " << SNAKE_SPEED << " m/s >" << "\t" << setiskin[1] << "\t" << name_skin << "\n";
     PrintMenuUnit("collision with edges", 3, vibor2);
-    if (dbarrier == 1) {
+    if (HAS_BARRIER == 1) {
         std::cout << "\t< YES >";
     }
     else {
@@ -434,7 +450,7 @@ void drawSettings(int vibor2, char skin[4], int bestscore, short need_record[10]
     }
     std::cout << "\t\t" << setiskin[2] << "\n";
     PrintMenuUnit("Skin of snake:", 4, vibor2);
-    std::cout << "\t\t<" << numskin << ">\n";
+    std::cout << "\t\t<" << SKIN_NUMBER << ">\n";
     PrintMenuUnit("Save game\n", 5, vibor2);
     PrintMenuUnit("Clear save\n", 6, vibor2);
     PrintMenuUnit("back\n", 7, vibor2);
@@ -444,7 +460,7 @@ void drawSettings(int vibor2, char skin[4], int bestscore, short need_record[10]
 
 
 
-void allmove(int vector, block snakeb[1024])
+void moveAll(int vector, block snakeb[1024])
 {
     int x_move = 0;
     int y_move = 0;
@@ -489,59 +505,41 @@ bool intersec(int lr, int ud, block snakeb[1024])
 
 
 
-void char_to_char_adder(char* canvas, const char str_for_add[])
-{
-    int i = 0;
-    int i2 = 0;
-    while (canvas[i] != '\0')
-    {
-        i++;
-    }
-    while (str_for_add[i2] != '\0')
-    {
-        canvas[i] = str_for_add[i2];
-        i++;
-        i2++;
-    }
-}
-
-
-
 int painter(int place[32][32], int score, char skin[4])
 {
     char* canvas = new char[3500]{ "" };
-    for (int i = 0; i < polsiz; i++)
+    for (int i = 0; i < FIELD_SIZE; i++)
     {
-        char_to_char_adder(canvas, "---");
+        addCharToChar(canvas, "---");
     }
-    char_to_char_adder(canvas, "--\tscore:");
-    char_to_int_adder(canvas, score);
-    char_to_char_adder(canvas, "\n");
-    for (int i = 0; i < polsiz; i++)
+    addCharToChar(canvas, "--\tscore:");
+    addIntToChar(canvas, score);
+    addCharToChar(canvas, "\n");
+    for (int i = 0; i < FIELD_SIZE; i++)
     {
-        char_to_char_adder(canvas, "|");
-        for (int j = 0; j < polsiz; j++)
+        addCharToChar(canvas, "|");
+        for (int j = 0; j < FIELD_SIZE; j++)
         {
             if (place[i][j] == 1)
             {
-                char_to_char_adder(canvas, skin);
+                addCharToChar(canvas, skin);
             }
             else if(place[i][j] == 2)
             {
-                char_to_char_adder(canvas, "(*)");
+                addCharToChar(canvas, "(*)");
             }
             else
             {
-                char_to_char_adder(canvas, "   ");
+                addCharToChar(canvas, "   ");
             }
         }
-        char_to_char_adder(canvas, "|\n");
+        addCharToChar(canvas, "|\n");
     }
-    for (int i = 0; i < polsiz; i++)
+    for (int i = 0; i < FIELD_SIZE; i++)
     {
-        char_to_char_adder(canvas, "---");
+        addCharToChar(canvas, "---");
     }
-    char_to_char_adder(canvas, "--\n");
+    addCharToChar(canvas, "--\n");
     std::cout << canvas;
     delete[] canvas;
     return 0;
@@ -549,27 +547,9 @@ int painter(int place[32][32], int score, char skin[4])
 
 
 
-bool long_press(int a, int* dolgnaz)
-{
-    if (GetAsyncKeyState(a) == -32768 && *dolgnaz != a)
-    {
-        *dolgnaz = a;
-        return 1;
-    }
-    else
-    {
-        return 0;
-    }
-}
-
-
-
 const char* ep_ask = "You are sure exit?\n";
 const char* cp_ask = "You are sure clear your save?\n";
-
-
-
-void ask_print(int choose, const char* text)
+void askPrint(int choose, const char* text)
 {
     system("cls");
     std::cout << text;
@@ -584,7 +564,7 @@ bool ask(const char* text)
     int choose = 1;
     while (true)
     {
-        ask_print(choose, text);
+        askPrint(choose, text);
         if (IsKeyJustPressed(87))
         {
             choose = CyclicSwitch(choose, 1, 2, false);
@@ -604,14 +584,12 @@ bool ask(const char* text)
 
 
 
-int snake(char skin[4], bool* snake_exit)
+int snakeProcess(char skin[4], bool* snake_exit)
 {
-    squareps = polsiz * polsiz;
-    bool first_launch = 1;
     int score = 0;
-    int timetoblock = 1000 / snakespeed;
-    int halfplace = polsiz / 2;
-    int lenth = 3;
+    int timetoblock = 1000 / SNAKE_SPEED;
+    int halfplace = FIELD_SIZE / 2;
+    int length = 3;
     int eat_resp = 1;
     int col = 0;
     int lr = 0;
@@ -622,9 +600,9 @@ int snake(char skin[4], bool* snake_exit)
     int a;
     int b;
     int place[32][32];
-    block* snakeb = new block[squareps];
+    block* snakeb = new block[FIELD_SIZE * FIELD_SIZE];
     i = 0;
-    while (i < lenth) //спавн змейки
+    while (i < length) //спавн змейки
     {
         snakeb[i].spawn(halfplace, halfplace + i);
         i++;
@@ -653,7 +631,7 @@ int snake(char skin[4], bool* snake_exit)
         }
         if (eat_resp == 1) //если еда была в прошлый раз съедена, переразместить еду и удлинится
         {
-            spawn_location_selection(&lr, &ud, place);
+            spawnLocationSelection(&lr, &ud, place);
             eat_resp = 0;
         }
         place[ud][lr] = 2;
@@ -692,7 +670,7 @@ int snake(char skin[4], bool* snake_exit)
                 }
             }
         }
-        col = dead(snakeb[0], place, vector);
+        col = checkCollide(snakeb[0], place, vector);
         if (col == 1)
         {
             return score;
@@ -701,12 +679,12 @@ int snake(char skin[4], bool* snake_exit)
         {
             timetoblock = timetoblock * 0.98;
             score++;
-            lenth++;
-            snakeb[lenth - 1].active = 1;
+            length++;
+            snakeb[length - 1].active = 1;
             eat_resp = 1;
         }
         col = 0;
-        allmove(vector, snakeb);
+        moveAll(vector, snakeb);
     }
     delete[] snakeb;
     return score;
@@ -714,7 +692,7 @@ int snake(char skin[4], bool* snake_exit)
 
 
 
-short skin_max(int bestscore, short need_record[10])
+short skinMax(int bestscore, short need_record[10])
 {
     short max_skin = 0;
     while (bestscore >= need_record[max_skin])
@@ -726,9 +704,9 @@ short skin_max(int bestscore, short need_record[10])
 
 
 
-bool skin_check(int bestscore, short need_record[10])
+bool skinCheck(int bestscore, short need_record[10])
 {
-    return bestscore >= need_record[numskin - 1];
+    return bestscore >= need_record[SKIN_NUMBER - 1];
 }
 
 
@@ -759,37 +737,37 @@ void settingsProcess(char* skin, int best_score, short* need_record) {
         {
             if (ask(cp_ask))
             {
-                clear(&best_score);
+                clearSave(&best_score);
             }
         }
         if (IsKeyJustPressed(13) && vibor2 == 5)
         {
-            sav(&best_score);
+            saveGame(&best_score);
         }
         if (IsKeyJustPressed(68) || IsKeyJustPressed(65))
         {
             switch (vibor2)
             {
             case 1:
-                polsiz = CyclicSwitch(polsiz, 16, 32, IsKeyJustPressed(68));
+                FIELD_SIZE = CyclicSwitch(FIELD_SIZE, 16, 32, IsKeyJustPressed(68));
                 break;
             case 2:
-                snakespeed = CyclicSwitch(snakespeed, 2, 10, IsKeyJustPressed(68));
+                SNAKE_SPEED = CyclicSwitch(SNAKE_SPEED, 2, 10, IsKeyJustPressed(68));
                 break;
             case 3:
-                dbarrier = CyclicSwitch(dbarrier, 0, 1, IsKeyJustPressed(68));
+                HAS_BARRIER = CyclicSwitch(HAS_BARRIER, 0, 1, IsKeyJustPressed(68));
                 break;
             case 4:
-                numskin = CyclicSwitch(numskin, 1, 10, IsKeyJustPressed(68));
+                SKIN_NUMBER = CyclicSwitch(SKIN_NUMBER, 1, 10, IsKeyJustPressed(68));
                 break;
             }
         }
         system("cls");
         vibvect = 1;
     }
-    if (skin_check(best_score, need_record) == 0)
+    if (skinCheck(best_score, need_record) == 0)
     {
-        numskin = skin_max(best_score, need_record);
+        SKIN_NUMBER = skinMax(best_score, need_record);
         setskin(skin);
     }
     if (exit == true)
@@ -809,7 +787,7 @@ int main()
     int score = 0;
     int vibor = 1;
     bool exit = false;
-    lod(&best_score);
+    loadGame(&best_score);
     srand(time(0));
     setskin(skin);
     while (1)
@@ -837,7 +815,7 @@ int main()
             {
                 if (vibor == 1)
                 {
-                    score = snake(skin, &snakeexit);
+                    score = snakeProcess(skin, &snakeexit);
                 }
                 if (vibor == 2)
                 {
@@ -866,7 +844,7 @@ int main()
                 }
                 if (vibor == 5)
                 {
-                    sav(&best_score);
+                    saveGame(&best_score);
                     return 0;
                 }
             }
@@ -889,7 +867,7 @@ int main()
             const char* owr_txt_4 = "X  X X  X X X X X      X  X  X  X X    X    \n";
             const char* owr_txt_5 = "XXXX X  X X X X XXXX   XXXX   XX  XXXX X    \n";
             std::cout << owr_txt_1 << owr_txt_2 << owr_txt_3 << owr_txt_4 << owr_txt_5;
-            if (score >= polsiz * polsiz)
+            if (score >= FIELD_SIZE * FIELD_SIZE)
             {
                 std::cout << "\n\nYou won! You have reached the maximum value on this place!";
             }
